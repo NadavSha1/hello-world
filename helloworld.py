c50a9b16f625a700f4586abf574cc37577093a17
NadavAlice <143273028+NadavAlice@users.noreply.github.com>
2023-08-28 11:17:59 +0300

    edit 4

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,9 @@
-def function (n1, n2):
+from random import randint
+
+def function():
+    n1 = randint(1, 5)
+    n2 = randint(6, 10)
     return n1, n2
 
-print(function(1,2))
-
-print(function(3,5))
+n1, n2 = function()
+print(n1, n2)
