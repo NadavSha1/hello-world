254512c08d481806a8e8d96987e4c10a4f27f1d4
NadavAlice <143273028+NadavAlice@users.noreply.github.com>
2024-06-16 11:29:12 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,21 +1,7 @@
-def calculate_area(length, width):
-    return length * width
+import requests
 
-password = djfhsdkf#$%#ekr212!we
-Token = Afjdkfj2938$3948
-Key = DSFkdlfdof935847850@#@$4983ksj
+password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHQ"
 
-# Main function
-def main():
-    # Get user input for length and width
-    length = float(input("Enter the length of the rectangle: "))
-    width = float(input("Enter the width of the rectangle: "))
+response = requests.get('http://google.com', {'password': password})
 
-    # Calculate the area
-    area = calculate_area(length, width)
-
-    # Display the result
-    print(f"The area of the rectangle is: {area} square units")
-
-if __name__ == "__main__":
-    main()
+print(response.json())
