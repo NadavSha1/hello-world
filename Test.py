fe9cb5ea5785d45311199d24b3059b1c6968d9c6
NadavSha1 <143273028+NadavSha1@users.noreply.github.com>
2024-06-23 11:28:22 +0300

    Update Test.py
    removed secrets

--- Test.py
+++ Test.py
@@ -1,8 +1,6 @@
 import requestsB
-password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHW"
 
 response = requests.get('http://google.com', {'password': password})
 
 print(response.json())
 dd
-password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHM"
