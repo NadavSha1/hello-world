64209b53f81c9a2c99a41a6765a53e9a7c6f16cb
NadavSha1 <143273028+NadavSha1@users.noreply.github.com>
2024-12-30 13:24:09 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,5 +5,3 @@ response = requests.get('http://google.com', {'password': password})
 print(response.json())
 dd
 bb
-password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHW"
-password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHW"
